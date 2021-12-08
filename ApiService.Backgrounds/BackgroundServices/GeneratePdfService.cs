1bf5f091507c491fbc86c52f595c89271410aaf3
WaineAlvesCarneiro <61710022+WaineAlvesCarneiro@users.noreply.github.com>
2021-12-08 09:53:13 -0300

    Add project files.

---
+++ ApiService.Backgrounds/BackgroundServices/GeneratePdfService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class GeneratePdfService : BackgroundService
+{
+    private readonly ILogger<GeneratePdfService> _logger;
+
+    public GeneratePdfService(ILogger<GeneratePdfService> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation(" ----> Começando a gerar Pdf. ----> ");
+        stoppingToken.Register(() => _logger.LogInformation("O serviço de demonstração está parando."));
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation(" ----> Gerando pdf em background. ----> ");
+            await GeneratePdf();
+        }
+        _logger.LogInformation(" ----> Pdf gerado. ----> ");
+    }
+
+    private async Task GeneratePdf()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(5));
+    }
+}
---
+++ ApiService.Hosteds/HostedService/TimerHostedService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class TimerHostedService : IHostedService
+{
+    private readonly ILogger _logger;
+
+    public TimerHostedService(ILogger<TimerHostedService> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        new Timer(ExecuteProcess, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+        return Task.CompletedTask;
+    }
+
+    private void ExecuteProcess(object state)
+    {
+        _logger.LogInformation(" ----> Execução do processo. ----> ");
+        _logger.LogInformation($"{DateTime.Now}");
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation(" <---- Parando o processo. <---- ");
+        _logger.LogInformation($"{DateTime.Now}");
+        return Task.CompletedTask;
+    }
+}
